1e104746b6678a1e8bc64c57308701a1ebc08555
DeeAnn Kendrick <deeannkcooper@bellsouth.net>
2016-06-30 07:40:37 -0400

    refactored duplicate code

--- server.js
+++ server.js
@@ -2,74 +2,82 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var socket = io;
+var clientSocket = io;
 var secretword;
 app.use(express.static('public'));
 
 
-io.on('connection', function(socket) {
+// io.on('connection', function(clientSocket) {
+//   console.log('a user connected');
+//   clientSocket.on("server-drawing", function(drawingData) {
+//     clientSocket.broadcast.emit("client-drawing",drawingData);
+//   });
+//   clientSocket.on('server-reset', function() {
+//     // console.log("I should be resetting");
+//     io.emit('client-reset');
+//   });
+// });
+
+
+
+// All the functionality for the chat:
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+// connection event is triggered when the io() is called
+// on the client side
+io.on('connection', function(clientSocket){
   console.log('a user connected');
-  socket.on("server-drawing", function(drawingData) {
-      socket.broadcast.emit("client-drawing",drawingData);
-      });
-      socket.on('server-reset', function() {
-        // console.log("I should be resetting");
-        io.emit('client-reset');
-      });
-    });
+
+  clientSocket.on("server-drawing", function(drawingData) {
+    clientSocket.broadcast.emit("client-drawing",drawingData);
+  });
+  clientSocket.on('server-reset', function() {
+    // console.log("I should be resetting");
+    io.emit('client-reset');
+  });
+
+  clientSocket.on('client-join', function(username) {
+    clientSocket.username = username;
+    clientSocket.broadcast.emit('join', username);
+  });
+  // disconnect event is triggered when the user closes
+  // the browser tab, for example
+  clientSocket.on('disconnect', function() {
+    io.emit('leave', clientSocket.username);
+  });
+
+
+  clientSocket.on('clientSecretWord', function(clientSecretWord){
+    secretword = clientSecretWord;
+    console.log(secretword);
+  });
+
+  // listen for chat messages
+  clientSocket.on('message-to-server', function(msg){
+    if (msg.message.indexOf(secretword)!== -1) {
+      console.log("winner");
+      io.emit("Winner");
+    } else {
+      clientSocket.broadcast.emit('message-to-client', msg);
+    }
+  });
+
+  clientSocket.on('start-type', function(username) {
+    clientSocket.broadcast.emit('start-type', username);
+  });
+
+  clientSocket.on('end-type', function(username) {
+    clientSocket.broadcast.emit('end-type', username);
+  });
 
 
 
-    // All the functionality for the chat:
-    app.get('/', function(req, res){
-      res.sendFile(__dirname + '/index.html');
-    });
-
-    // connection event is triggered when the io() is called
-    // on the client side
-    io.on('connection', function(clientSocket){
-      console.log('a user connected');
-
-      clientSocket.on('client-join', function(username) {
-        clientSocket.username = username;
-        clientSocket.broadcast.emit('join', username);
-      });
-      // disconnect event is triggered when the user closes
-      // the browser tab, for example
-      clientSocket.on('disconnect', function() {
-        io.emit('leave', clientSocket.username);
-      });
+});
+// This ends the functionality of the chat
 
 
-      clientSocket.on('clientSecretWord', function(clientSecretWord){
-        secretword = clientSecretWord;
-        console.log(secretword);
-      });
-
-      // listen for chat messages
-      clientSocket.on('message-to-server', function(msg){
-        if (msg.message.indexOf(secretword)!== -1) {
-          console.log("winner");
-          io.emit("Winner");
-        } else {
-        clientSocket.broadcast.emit('message-to-client', msg);
-        }
-      });
-
-      clientSocket.on('start-type', function(username) {
-        clientSocket.broadcast.emit('start-type', username);
-      });
-
-      clientSocket.on('end-type', function(username) {
-        clientSocket.broadcast.emit('end-type', username);
-      });
-
-
-
-    });
-    // This ends the functionality of the chat
-
-
-    http.listen(8000, function(){
-      console.log('Listening on 8000');
-    });
+http.listen(8000, function(){
+  console.log('Listening on 8000');
+});
